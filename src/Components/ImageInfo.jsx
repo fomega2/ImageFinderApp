16259d1a5d234b32238075c7023169aa364f7eb5
Fabián Calderón Vindas <fcalderon@ebs-sys.com>
2023-04-07 21:58:54 -0600

    Add scroll pagination

--- src/Components/ImageInfo.jsx
+++ src/Components/ImageInfo.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import LazyLoad from 'react-lazy-load'
 
-export const ImageInfo = ({imgInfo}) => {      
+export const ImageInfo = ({imgInfo}) => {        
   return (    
     <div className="bg-white rounded-lg overflow-hidden shadow-md mx-1 mt-2 flex opacity-90 hover:opacity-100">
-      <img src={imgInfo.urls.small }  className="w-full object-cover"/>      
+      <LazyLoad>
+        <img src={imgInfo.urls.small }  className="w-full object-cover" alt={imgInfo.alt_description}/>      
+      </LazyLoad>      
     </div>
   )
 }
--- src/Components/ListImages.jsx
+++ src/Components/ListImages.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { ImageInfo } from './ImageInfo'
 export const ListImages = ({images}) => {    
   return (
-    <div className='flex justify-center'>
+    <div className='flex justify-center mb-52'>
       <div className='flex flex-wrap justify-center w-4/5 mt-'>
           {
             images.length > 0 && images.map((imgInfo,i) => (
